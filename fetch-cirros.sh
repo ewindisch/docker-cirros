dc7984c4787d56186ee9e5143bceb3b8852c355a
Odidev <odidev@puresoftware.com>
2018-07-13 12:02:41 +0530

    Add support for ARM64V8 architecture
    Signed-off-by: Odidev <odidev@puresoftware.com>

--- fetch-cirros.sh
+++ fetch-cirros.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-VERSION=0.3.4
-URL=https://download.cirros-cloud.net/${VERSION}/cirros-${VERSION}-x86_64-lxc.tar.gz
+VERSION=0.4.0
+ARCH="$(uname -m)"
+URL=https://download.cirros-cloud.net/${VERSION}/cirros-${VERSION}-${ARCH}-lxc.tar.gz
 wget $URL
+sed -i "s/cirros-0.3.4-x86_64-lxc.tar.gz/cirros-$VERSION-$ARCH-lxc.tar.gz/g" Dockerfile
